3ad3c0b633d71ab54211ba8c39ec05d3f65e0514
Hamza <h.m.ashiblie@gmail.com>
2017-07-22 22:20:27 +0300

    view

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,19 +11,22 @@
 |
 */
 Route::group(['middleware'=>['web']],function (){
-    Route::get('/', function () {
-        return view('welcome');
-    });
-    Route::post('/signup',[
-        'uses'=>'UserController@postSignUp',
-        'as'=> 'signup'
-    ]);
-    Route::post('/signin',[
-        'uses'=>'UserController@postSignIn',
-        'as'=> 'signin'
-    ]);
-    Route::get('/dashboard',[
-        'uses'=>'UserController@getDashboard',
-        'as'=> 'dashboard'
-    ]);
+
 });
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+Route::post('/signup',[
+    'uses'=>'UserController@postSignUp',
+    'as'=> 'signup'
+]);
+Route::post('/signin',[
+    'uses'=>'UserController@postSignIn',
+    'as'=> 'signin'
+]);
+Route::get('/dashboard',[
+    'uses'=>'UserController@getDashboard',
+    'as'=> 'dashboard',
+    'middleware'=> 'auth'
+]);
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,61 @@
-<h3>
-    Dashboard
-</h3>
+@extends('layouts.master')
+
+@section('content')
+
+    <section class="row new-post">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>What do you want to say?</h3></header>
+            <form action="">
+                <div class="form-group">
+                    <textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Your Post"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Create Post</button>
+            </form>
+        </div>
+
+    </section>
+    <section class="row posts">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>What other people say?</h3></header>
+            <article class="post">
+                <p>
+                    dummy text here dummy text here dummy text here dummy text here dummy text here dummy text here dummy text here
+                    dummy text here dummy text here dummy text here dummy text here dummy text here dummy text here dummy text here
+                    dummy text here dummy text here dummy text here dummy text here dummy text here dummy text here dummy text here
+                    dummy text here dummy text here dummy text here dummy text here dummy text here dummy text here dummy text here
+
+                </p>
+                <div class="info">
+                    Posted by Ash on 20 JUL 2017
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a>|
+                    <a href="#">Disike</a>|
+                    <a href="#">Edit</a>|
+                    <a href="#">Delete</a>
+                </div>
+            </article>
+            <article class="post">
+                <p>
+                    dummy text here dummy text here dummy text here dummy text here dummy text here dummy text here dummy text here
+                    dummy text here dummy text here dummy text here dummy text here dummy text here dummy text here dummy text here
+                    dummy text here dummy text here dummy text here dummy text here dummy text here dummy text here dummy text here
+                    dummy text here dummy text here dummy text here dummy text here dummy text here dummy text here dummy text here
+
+                </p>
+                <div class="info">
+                    Posted by Ash on 20 JUL 2017
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a>|
+                    <a href="#">Disike</a>|
+                    <a href="#">Edit</a>|
+                    <a href="#">Delete</a>
+                </div>
+            </article>
+
+        </div>
+
+    </section>
+
+@endsection
